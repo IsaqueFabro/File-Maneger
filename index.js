27443999aba6d686d56b1936b226d006ea42adb1
“Isaque <isaquefabro04@gmail.com>
2025-07-29 17:57:22 -0300

    user interaction by terminal in construction, new dependencies implemented

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-const fileManager = require('./fileManager');
+const fileManeger = require("./fileManeger");
+const readlineSync = require("readline-sync");
+const path = require("path");
 
-fileManager
+function main() {
+  while (true) {
+    console.log("\nMenu");
+    console.log("1. Create file");
+    console.log("2. List files");
+    console.log("3. Read file");
+    console.log("4. Write to file");
+    console.log("5. Delete file");
+    console.log("6. Exit");
+
+    const choice = readlineSync.question("Choose an option: ");
+
+    switch (choice) {
+        case '1':
+            console.log("File created successfully.");
+            break;
+        case '2':
+            console.log("Listing files...");
+            break;
+        case '3':
+            
+
+  }
+}
+
+main();
